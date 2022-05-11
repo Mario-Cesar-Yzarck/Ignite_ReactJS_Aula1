fbed7d50f3b032bf0f50b3e26cd160226a0f5f05
Mario <mc.fialho@hotmail.com>
2022-05-11 16:42:22 -0300

    instal and config typescript

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -26,3 +26,28 @@ export function Counter() {
 o useState no react faz com que o valor daquela variável seja observado pelo react, caso não use isso ele fica inalterado.
 os [] após a declaração da const é o destruction e as duas palavras dentro dele servem como valor atual e alterado.
 */
+
+/* Entendendo um pouco de typeScript
+
+type User = {
+    name: string
+    email: string
+    address: {
+        city: string
+        state?: string
+    }
+}
+
+function showWelcomeMessage(user: User) {
+    return `Welcome ${user.name}, email ${user.email} e adress ${user.address.city}`
+}
+
+showWelcomeMessage({
+    name: 'Jon',
+    email: 'oi@oi.com',
+    address: {
+        city: 'Ecops',
+        state: 'ES',
+    }
+})
+*/
